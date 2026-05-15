bf11d6581ff744dc458535f5a5963238d8845cd8
tilak <tilaksasmal999@gmail.com>
2026-05-15 11:20:29 +0000

    feat: add image cropper component using react-konva

--- src/editor/page.tsx
+++ src/editor/page.tsx
@@ -1,12 +1,10 @@
-import { Editor } from "@/components/editor";
 import "../../styles/index.css";
-import { Button } from "@/components/ui/button";
+import ImageCropper from "@/components/cropper";
 
 export function App() {
   return (
-    <div className="bg-black/60 backdrop-blur-sm z-50 fixed w-full h-full inset-0">
-      <button>Hi</button>
-      <Button>Hello</Button>
+    <div className="bg-black/90 backdrop-blur-sm z-50 fixed w-full h-full inset-0">
+      <ImageCropper imageUrl="https://images.unsplash.com/photo-1778343303023-c6404b185480?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
     </div>
   );
 }
